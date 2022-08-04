459dbb8cad3898ea45300c2645f9bc0a78583b42
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2022-08-04 20:42:52 +0200

    popup adjustments

--- main/popup.ts
+++ main/popup.ts
@@ -1,35 +1,32 @@
 export const makePopup = () => {
-    const elem = document.createElement('div');
-    elem.id = 'make-money-with-us';
-    // elem.classList.add('hidden');
-    elem.innerHTML = `
-    <div class="make-money-with-us">
-        <div class='cover'>
-        <div class="make-money-with-us-header">
-            <h1>Make Money With Us</h1>
-            <div class="make-money-with-us-close">
-
-                <i class="fas fa-times"></i>
-            </div>  
+    const elem  = `
+    <div class="popup-inner h-3/4 flex h-full justify-center md:px-24 md:pt-34 md:mt-24 lg:px:24 px-4 items-center ">
+    <div class="popup-content bg-white relative rounded-lg p-3 w-full flex justify-center items-center h-full h-full p-4">
+      <div class="popup-header">
+        <div class="popup-title md:text-6xl lg:text-6xl text-3xl">
+          <p>SIGN UP AND LEARN</p>
+          <p class="px-6 py-2 text-red-400 ">18 MODERN WAYS TO MAKE MONEY ONLINE</p>
+          <p class="text-right underline underline-green">FOR ONLY $45</p>
         </div>
-        <div class="make-money-with-us-body">
-
-            <div class="make-money-with-us-body-content">   
-                <div class="make-money-with-us-body-content-header">
-                    <h2>Make Money With Us</h2>
-                    <p>
-                       SIGN UP AND LEARN 18 MODERN WAYS ON HOW TO MAKE MONEY ONLINE FOR ONLY $45
-                    </p>
-                </div>
-             
-            </div>
-        </div>
-        </div>
+        <button class="popup-close absolute top-0 m-3 right-0">
+          <svg class="h-6 w-6 " fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd"
+              d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
+              clip-rule="evenodd" />
+          </svg>
+        </button>
+       <div class="text-center flex justify-center items-center  pt-5 mt-6">
+   
+        <a class=" bg-yellow-600 text-white heartbeat rounded-lg p-3  text-3xl lg:w-1/2" href="/learn.html"> Get started here</a>
+       </div>
+      </div>
     </div>
+  </div>
     `;
-    document.body.appendChild(elem);
-    const makeMoneyWithUs = document.getElementById('make-money-with-us');
-    const makeMoneyWithUsClose = document.getElementsByClassName('make-money-with-us-close')[0];
+ 
+    const makeMoneyWithUs = document.getElementById('money-cta');
+    makeMoneyWithUs.innerHTML= elem
+    const makeMoneyWithUsClose = document.getElementsByClassName('popup-close')[0];
     makeMoneyWithUsClose.addEventListener('click', () => {
         makeMoneyWithUs?.classList.add('hidden');
     }

f86a5ed98397cf6b05eedffde08ad7735c543482
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2022-08-04 23:39:33 +0200

    add css styles for the popup

--- main/popup.ts
+++ main/popup.ts
@@ -4,20 +4,20 @@ export const makePopup = () => {
     <div class="popup-inner  ">
     <div class="popup-content ">
       <div class="popup-header">
-        <div class="popup-title md:text-6xl lg:text-6xl text-3xl">
+        <div class="popup-title ">
           <p>SIGN UP AND LEARN</p>
-          <p class=" px-6 py-2 text-red-400 ">18 MODERN WAYS TO MAKE MONEY ONLINE</p>
-          <p class="text-right underline underline-green">FOR ONLY $45</p>
+          <p class=" red-txt  ">18 MODERN WAYS TO MAKE MONEY ONLINE</p>
+          <p class="ltitle">FOR ONLY $45</p>
         </div>
-        <button class="popup-close absolute top-0 m-3 right-0">
-          <svg class="h-6 w-6 " fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <button class="popup-close ">
+          <svg class="po-close-icon " fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd"
               d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
               clip-rule="evenodd" />
           </svg>
         </button>
-       <div class="text-center flex justify-center items-center  pt-5 mt-6">
-        <a class=" bg-yellow-600 text-white heartbeat rounded-lg p-3  text-3xl lg:w-1/2" href="/learn.html"> Get started here</a>
+       <div class="po-started-btn">
+        <a class=" po-btn-link heartbeat" href="/learn.html"> Get started here</a>
        </div>
       </div>
     </div>

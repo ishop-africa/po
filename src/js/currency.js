bafa330fd478872cd69805b8a8363a1ad1a57d3f
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2022-08-04 11:44:47 +0200

    working module

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,16 +1,4 @@
-const curencies = async () => {
-    const sessionAmount = sessionStorage.getItem('poAmmount')
-    if (sessionAmount) {
-        return sessionAmount;
-    }else {
-        const data = await fetch('https://api.exchangerate.host/convert?from=USD&to=ZAR&amount=45')
-        const f = await data.json()
-        sessionStorage.setItem("poAmmount", f.result)
-        return f;
-    }
-}
 
-curencies();
 
 // export default curencies
 

1fbf1b3487adebd3772887283ac1e7cbeb7dbbaf
Mitch Chimwemwe Chanza <mitch.chanza@gmail.com>
2022-08-09 11:42:26 +0200

    updated

--- main/estore.ts
+++ main/estore.ts
@@ -27,7 +27,7 @@ export const EshopPayments = async () => {
     shorpingCart.className = "container"
     shorpingCart.id = "shoppingCart"
     shorpingCart.innerHTML = `
-    <div class="cartContanier" id='cartContanier' data-toggle="modal" data-target="#mbr-popup-ji">
+    <div class="cartContanier" id='cartContanier'>
         <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
         </svg>

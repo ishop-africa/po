d143dee69001b33e0603e48fe25cb0d806df56b7
Mitch Chimwemwe Chanza <66210356+mitch1009@users.noreply.github.com>
2022-08-09 19:32:15 +0200

    Delete assets/yapee directory

--- assets/yapee/main.js
+++
@@ -1,6 +0,0 @@
-(async ()=> {
-    const script = document.createElement('script');
-    script.src = './estore.js'; // chnage this to a cdn version
-    document.body.appendChild(script);
-    console.log('From Yappee')
-})()
